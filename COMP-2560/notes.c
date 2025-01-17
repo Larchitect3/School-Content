30ce2295bcf760e74239d7498a6107310ffaf17f
Tresor Dan Minor <93678681+Larchitect3@users.noreply.github.com>
2025-01-16 19:56:00 -0500

    added files

---
+++ COMP-2560/notes.c
@@ -0,0 +1,31 @@
+// Winter 2025
+//------------
+//(mkdir "filename.ext") : makes a new folder/directory
+//(cd "filename.ext") : change directory
+//  (echo "message" > "filename.txt" ): writes the message to the file and saves it in the current folder. However, if you rewrite a new message, it erases the previous one.
+// A program is an executable file that resides on a disk
+// A process is an executing program usually with a limited life time. A process is made from a process.
+// A process ID (PID) is a unique nonnegative 
+// getpid() system call gets you the PID of the process.
+// getuid() gets you the user ID of the process.
+// ctrl+D closes the shell terminal.
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
